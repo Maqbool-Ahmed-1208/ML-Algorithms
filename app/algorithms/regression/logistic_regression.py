509bde892ab09af5749a87fc22d2649d51687fa6
Maqbool-Ahmed-1208 <maqboolshaikh321@gmail.com>
2025-10-16 22:44:28 +0500

    updates in logistic regression code

--- app/algorithms/regression/logistic_regression.py
+++ app/algorithms/regression/logistic_regression.py
@@ -1,11 +1,18 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-def logistic_regression(x, y, test_size = 0.2, random_state = 42):
+
+def logistic_regression(x, y, test_size = 0.2, random_state = 42, no_iter = 1000):
+    
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = test_size, random_state = random_state)
 
-    model = LogisticRegression(tol = 0.01, max_iter = 1000)
+    model = LogisticRegression(tol = 0.0001, max_iter = no_iter, random_state=random_state)
     model.fit(X_train,y_train)
 
     y_pred = model.predict(X_test)
-    accuracy_score(y_test, y_pred)*100
+    accuracy = accuracy_score(y_test, y_pred)*100
+    print("accuracy = ", accuracy)
+
+    return y_pred, accuracy
+
+
