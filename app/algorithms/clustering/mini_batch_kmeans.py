ed81d3bbf8c9c450dfd34d1ab56749c59db0fe96
Maqbool-Ahmed-1208 <maqboolshaikh321@gmail.com>
2025-10-22 23:24:42 +0500

    mini batch kmeans updated

--- app/algorithms/clustering/mini_batch_kmeans.py
+++ app/algorithms/clustering/mini_batch_kmeans.py
@@ -6,13 +6,15 @@ from sklearn.preprocessing import StandardScaler
 
 
 
-def mini_batch_kmeans(df, no_clusters = 3, batch_size=8)
+def mini_batch_kmeans(df, no_clusters = 3, batch_size=8):
 
-    mbkmeans =  MiniBatchKMeans(n_clusters=no_clusters, batch_size=8)
-
-
+    mbkmeans =  MiniBatchKMeans(n_clusters=no_clusters, batch_size=batch_size)
 
     df['Cluster'] = mbkmeans.fit_predict(df)
 
+    # cluster_labels = {df['Cluster'].min() : "weak"  , df['Cluster'].max() : "moderate", df['Cluster'].median() : "strong"}
+    last_index = len(df.columns)  - 1 
 
-    cluster_labels = {df['Cluster'].min() : "weak"  , df_selected['Cluster'].max() : "moderate", df_selected['Cluster'].median() : "strong"}
+    plt.figure(figsize = (20,10))
+
+    plt.scatter(df.iloc[:last_index], [0]*len(df), c = df['Cluster'], cmap='rainbow')
